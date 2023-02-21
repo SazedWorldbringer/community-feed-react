d8df108cb28ca7ffef4e8ba8fc1262ffc898c71d
Atharva Pardeshi <atharva.exe@gmail.com>
2023-02-21 17:51:58 +0530

    feat: fetch questions containing the reactjs tag and render them using the Card component

--- pages/questions/index.js
+++ pages/questions/index.js
@@ -1,3 +1,5 @@
+import Card from "@/components/Card";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 
 const QuestionsContainer = styled.div`
@@ -8,9 +10,40 @@ const QuestionsContainer = styled.div`
 `;
 
 function Questions() {
+  const [loading, setLoading] = useState(true);
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await fetch(
+        'https://api.stackexchange.com/2.2/questions?order=desc&sort=hot&tagged=reactjs&site=stackoverflow',
+      );
+      const result = await data.json();
+      if (result) {
+        setQuestions(result.items);
+        setLoading(false)
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
     <QuestionsContainer>
-    <h2>Qustions</h2>
+      <h2>Qustions</h2>
+      {loading ? (
+        <span>Loading...</span>
+      ) : (
+        <div>
+          {questions?.map((question) => (
+            <Card
+              key={question.question_id}
+              title={question.title}
+              views={question.view_count}
+              answers={question.answer_count}
+            />
+          ))}
+        </div>
+      )}
     </QuestionsContainer>
   )
 }
